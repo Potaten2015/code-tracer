325dea2dc1dd776e6489fa0f5111ea25c60a51a1
Taten Knight <knight.taten@gmail.com>
2023-05-09 01:38:38 +0200

    hmmmmm

--- code_tracer/video_creator.py
+++ code_tracer/video_creator.py
@@ -23,14 +23,14 @@ SUPPORTED_LANGUAGES = {
     # Add more language mappings here
 }
 
-VIDEO_VARIATIONS = [("vertical", (2160, 3840)), ("horizontal", (3840, 2160))]
+VIDEO_VARIATIONS = [("vertical", (2160, 4096)), ("horizontal", (4096, 2160))]
 FPS = 60
 STYLE = get_style_by_name('bw')
 BACKROUND_COLOR = ImageColor.getrgb(STYLE.background_color)
 
 
-def get_language(filename):
-    file_extension = os.path.splitext(filename)[-1]
+def get_language(filepath):
+    file_extension = os.path.splitext(filepath)[-1]
     return SUPPORTED_LANGUAGES.get(file_extension, "text")
 
 
@@ -41,10 +41,11 @@ def highlight_code(code, language):
 
 
 def create_image(data, aspect_ratio):
-    code_image = np.frombuffer(highlight_code(data["content"], data["language"]), dtype=np.uint8)
+    extended_content = data["content"] + "\n" * (data["max_lines"] - data["total_lines"])
+    code_image = np.frombuffer(highlight_code(extended_content, data["language"]), dtype=np.uint8)
     code_image = cv2.imdecode(code_image, cv2.IMREAD_UNCHANGED)
 
-    # Add filename and project name to the image
+    # Add filepath and project name to the image
     text = f"""({data['github_username']}
     {data['project_name']})
     {data['filepath']}"""
@@ -52,19 +53,29 @@ def create_image(data, aspect_ratio):
     font_scale = 0.8
     font_thickness = 2
     text_size, _ = cv2.getTextSize(text, font, font_scale, font_thickness)
-    canvas_r = np.full((aspect_ratio[0], aspect_ratio[1]), dtype=np.uint8, fill_value=BACKROUND_COLOR[0])
-    canvas_g = np.full((aspect_ratio[0], aspect_ratio[1]), dtype=np.uint8, fill_value=BACKROUND_COLOR[1])
-    canvas_b = np.full((aspect_ratio[0], aspect_ratio[1]), dtype=np.uint8, fill_value=BACKROUND_COLOR[2])
+    max_code_height = aspect_ratio[1] - text_size[1]
+    canvas_r = np.full((aspect_ratio[1], aspect_ratio[0]), dtype=np.uint8, fill_value=BACKROUND_COLOR[0])
+    canvas_g = np.full((aspect_ratio[1], aspect_ratio[0]), dtype=np.uint8, fill_value=BACKROUND_COLOR[1])
+    canvas_b = np.full((aspect_ratio[1], aspect_ratio[0]), dtype=np.uint8, fill_value=BACKROUND_COLOR[2])
     canvas = np.stack([canvas_r, canvas_g, canvas_b], axis=-1)
-    if code_image.shape[1] > aspect_ratio[1]:
-        code_image = code_image[:, : aspect_ratio[1], :]
-    lines = len(code_image[1, :, 1])
+    code_image_slice = code_image
+    code_image_width = code_image.shape[1]
     i = 0
-    while lines > 0:
-        cut_image = code_image[
-            : aspect_ratio[0] - text_size[1], code_image.shape[1] * i : code_image.shape[1] * (i + 1), :
-        ]
-        canvas[text_size[1] : text_size[1] + code_image.shape[0], : code_image.shape[1], :] = cut_image
+    while code_image_slice.size != 0:
+        code_image_slice = code_image[i * max_code_height : (i + 1) * max_code_height, :, :]
+        if code_image_slice.size == 0:
+            break
+        if code_image_slice.shape[1] + (i * code_image_width) > aspect_ratio[0]:
+            code_image_slice = code_image_slice[:, : aspect_ratio[0] - (i * code_image_width), :]
+        try:
+            canvas[
+                text_size[1] : code_image_slice.shape[0] + text_size[1],
+                i * code_image_width : i * code_image_width + code_image_slice.shape[1],
+                :,
+            ] = code_image_slice
+        except:
+            print("error")
+        i = i + 1
     cv2.putText(canvas, text, (0, text_size[1]), font, font_scale, (0, 0, 0), font_thickness)
 
     return canvas
@@ -106,6 +117,13 @@ if __name__ == "__main__":
 def get_change_files(changes_dir):
     change_filenames = sorted(glob.glob(os.path.join(changes_dir, f"*")))
     change_files = [json.load(open(change_filename, "r")) for change_filename in change_filenames]
+    change_files = [
+        {**change_file, "total_lines": len(change_file["content"].split("\n"))} for change_file in change_files
+    ]
+    max_lines = {filepath: 0 for filepath in set([change_file["filepath"] for change_file in change_files])}
+    for change_file in change_files:
+        max_lines[change_file["filepath"]] = max(max_lines[change_file["filepath"]], change_file["total_lines"])
+    change_files = [{**change_file, "max_lines": max_lines[change_file["filepath"]]} for change_file in change_files]
     if change_files:
         return change_files
     else:
