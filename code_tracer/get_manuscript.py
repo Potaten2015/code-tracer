b24203c9d2e08f8c28d4829aee1cc168458f4815
Taten Knight <knight.taten@gmail.com>
2023-06-15 18:23:06 -0600

    slick

--- code_tracer/get_manuscript.py
+++ code_tracer/get_manuscript.py
@@ -9,8 +9,11 @@ load_dotenv()
 
 def create_payload(change_files, config, logger):
     logger.info("Creating GPT payload...")
-    changes = {filename: {"beginning": values[0]["content"], "end": values[-1]["content"]} for filename, values in change_files.items()}
-    
+    changes = {
+        filename: {"beginning": values[0]["content"], "end": values[-1]["content"]}
+        for filename, values in change_files.items()
+    }
+
     payload = {
         "video_catch_phrase": config.get("video_catch_phrase"),
         "total_progress": config.get("total_progress"),
@@ -40,16 +43,23 @@ def get_manuscript(payload, config, logger):
         messages=[
             {"role": "system", "content": context},
             {"role": "user", "content": json.dumps(payload)},
-        ]
+        ],
     )
-    response = completion.choices[0].message.content
-    response_json = json.loads(response)
+
+    output_location = config.get("video_output_dir")
+    gpt_api_response_filepath = os.path.join(output_location, 'gpt_api_response.json')
+    with open(gpt_api_response_filepath, "w") as out:
+        json.dump(completion, out, indent=4)
+        logger.info(f'GPT api response written to file: {gpt_api_response_filepath}')
+    content = completion.choices[0].message.content
+    gpt_content_filepath = os.path.join(output_location, 'gpt_content.json')
+    response_json = json.loads(content)
+    with open(gpt_content_filepath, "w") as out:
+        json.dump(response_json, out, indent=4)
+        logger.info(f'GPT message content written to file: {gpt_content_filepath}')
     config.set("total_progress", response_json["total_progress"])
     config.write()
-    output_location = config.get("video_output_dir")
-    output_file = os.path.join(output_location, 'gpt_response.json')
-    with open(output_file, "w") as out:
-        json.dump(response_json, out, indent=4)
-        logger.info(f'GPT response written to file: {output_file}')
+
     manuscript = response_json["manuscript"]
+    manuscript = manuscript.replace("`", "'")
     return manuscript
